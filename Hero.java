c1cbd296192c5f2c45a8dda4e5a28ce15a34d26f
Minigopal <archer.arjunraj@gmail.com>
2025-11-04 13:50:25 -0500

    added Hero subclass

---
+++ Hero.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Hero here.
+ * 
+ * @author Arjun Rajagopal 
+ * @version 11.04.2025
+ */
+public class Hero extends Actor
+{
+    /**
+     * Act - do whatever the Hero wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        
+    }
+}
