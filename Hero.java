94a392dca8b5816bd22c29c476769a9d64052e26
Minigopal <archer.arjunraj@gmail.com>
2025-11-04 14:41:21 -0500

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -8,13 +8,49 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Hero extends Actor
 {
-    /**
-     * Act - do whatever the Hero wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    int speed = 1;
+    int dx = 0;
+    int dy = 0;
     public void act()
     {
-        // Add your action code here.
+         if (Greenfoot.isKeyDown("w")){
+            dx = 0;
+            dy = -speed;
+        }
+        if (Greenfoot.isKeyDown("a")){
+            dx = -speed;
+            dy = 0;
+        }
+        if (Greenfoot.isKeyDown("s")){
+            dx = 0;
+            dy = speed;
+        }
+        if (Greenfoot.isKeyDown("d")){
+            dx = speed;
+            dy = 0;
+        }
         
+        if (Greenfoot.isKeyDown("up")){
+            speed += 1;
+        }
+        if (Greenfoot.isKeyDown("down") && speed > 0){
+            speed -= 1;
+        }
+        if (getX() > 550){
+            setLocation(-200, getY());
+        }
+        if (getX() < 50){
+            setLocation(350, getY());
+        }
+        if (getY() > 350){
+            setLocation(getX(), 50);
+        }
+        if (getY() < 50){
+            setLocation(getX(), 550);
+        }
+        
+        
+        
+        setLocation(getX() + dx, getY() + dy);
     }
 }
